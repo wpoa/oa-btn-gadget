b0da25f49537bf92dfda6e5f4acc8ac8f3cb85ff
Max Klein <isalix@gmail.com>
2014-11-24 12:57:28 -0800

    Using Anthony's version

--- oa-btn-gadget.js
+++ oa-btn-gadget.js
@@ -1,115 +1,113 @@
-/*
-  Open Access Button integration gadget
-*/
+var accessIsAlreadyOpenPrefixes = new Array(
+	'10.1098', '10.1155', '10.1186', '10.1371', '10.1534', '10.2196', '10.3352', '10.3389', '10.3390',
+	'10.3814', '10.3897', '10.4061', '10.5194', '10.5402', '10.6064', '10.7167', '10.7554', '10.7717'
+	);
+var refLinkSelector = "a";
+var doiRegex = /dx\.doi\.org\/(.*)/g;
+var links = document.querySelectorAll(refLinkSelector);
+var doiLinks = new Array();
+for (i = 0; i < links.length; i++) {
+	var link = decodeURIComponent(links[i].href);
+	var match = doiRegex.exec(link);
+	if (match != null && accessIsAlreadyOpenPrefixes.indexOf(match[1].slice(0, 8)) == -1) {
+		doiLinks.push(link);
+	}
+}
+console.log(doiLinks);
 
-(function() {
-
-    function showOAButtonPopup(doi) {
-        // ascertain if the bookmarklet has already been called
-        var exists = document.getElementById("OAButton");
-
-        if(exists == null) {
-
-            // TODO we won't have a custom user url for wikipedia users but OA button won't work without one
-            var url = "https://www.openaccessbutton.org/api/form/page1/689e8f328d9a47c58755c8c80918da57/";
-
-            if(doi) {
-                url += "?doi=" + encodeURIComponent(doi);
-            }
-
-            // build the control div
-            var div = document.createElement("div");
-		        div.setAttribute("allowTransparency", "true");
-		        div.setAttribute("id", "OAButton");
-
-		        div.style.position = "fixed";
-		        div.style.zIndex = "2147483640";
-		        div.style.boxSizing = "border-box";
-		        div.style.MozBoxSizing = "border-box";
-		        div.style.padding = "15px";
-		        div.style.background = "white";
-		        div.style.height = "100%";
-		        div.style.width = "350px";
-		        div.style.top = "0";
-		        div.style.right = "0";
-		        div.style.overflow = "scroll";
-		        div.style.overflowX = "hidden";
-
-		        document.body.appendChild(div);
-
-		        // add the close button
-		        var closeButton = document.createElement("a");
-		        closeButton.setAttribute("href", "javascript:document.getElementById('OAButton').setAttribute('style', 'display:none')");
-		        closeButton.setAttribute("id", "closeButton");
-		        closeButton.appendChild(document.createTextNode("X"));
-		        closeButton.style.zIndex = "2147483641";
-		        closeButton.style.position = "relative";
-		        closeButton.style.top = "0";
-
-		        div.appendChild(closeButton);
-
-		        // add the iframe
-		        var iframe = document.createElement("iframe");
-		        iframe.setAttribute("allowTransparency", "true");
-		        iframe.setAttribute("src", url);
-
-		        iframe.style.position = "fixed";
-		        iframe.style.zIndex = "2147483640";
-		        iframe.style.boxSizing = "border-box";
-		        iframe.style.MozBoxSizing = "border-box";
-		        iframe.style.padding = "15px";
-		        iframe.style.borderLeft = "2px #555 dashed";
-		        iframe.style.background = "white";
-		        iframe.style.height = "100%";
-		        iframe.style.width = "350px";
-		        iframe.style.bottom = "0";
-		        iframe.style.right = "0";
-
-		        div.appendChild(iframe);
-	      } else {
-		        var div = exists;
-		        div.setAttribute("allowTransparency", "true");
-		        div.setAttribute("id", "OAButton");
-
-		        div.style.position = "fixed";
-		        div.style.zIndex = "2147483640";
-		        div.style.boxSizing = "border-box";
-		        div.style.MozBoxSizing = "border-box";
-		        div.style.padding = "15px";
-		        div.style.background = "white";
-		        div.style.height = "100%";
-		        div.style.width = "350px";
-		        div.style.top = "0";
-		        div.style.right = "0";
-		        div.style.overflow = "scroll";
-		        div.style.overflowX = "hidden";
-		        div.style.display = "block";
-
-	      }
+function oabutton(options) {
+    var about = {
+        version: 0.1,
+        author: "Mark MacGillivray",
+        created: "01102014",
+        description: "A javascript client that operates against the Open Access Button 2.0 API"
     }
+    var defaults = {
+        api: 'http://oabutton.cottagelabs.com/api',
+        api_key: ''
+    }
+    this.options = $.extend(defaults, options);
+    this.response = {};
+}
 
-
-    var a = $('span.citation');
-    var i, oabtn;
-    $.each(a, function(i, el) {
-        el = $(el)
-        // is this a doi?
-        if(!el.children("a[title='Digital object identifier']")) {
-            console.log("skipping");
-            return true; // if not a doi, skip
+oabutton.prototype = {
+    send: function(action,o) {
+        this.options.api_key && !o.data.api_key ? o.data.api_key = this.options.api_key : false;
+        var vars = {
+            type: 'POST',
+            url: this.options.api + '/' + action,
+            contentType: 'application/json',
+            dataType: 'JSON',
+            processData: false,
+            //crossDomain: true,
+            cache: false,
+            context: this,
+            data: JSON.stringify(o.data)
         }
-        var doi = el.children('a.external').html();
-        // TODO should check if a "non-oa" css class is set on el
-        if(true) { 
-            oabtn = document.createElement('a');
-            oabtn.setAttribute('href', '#');
-            oabtn.innerHTML = "Report paywall";
-            $(oabtn).click(function(e) {
-                showOAButtonPopup(doi);
-                return false;
-            })
-            el.append(oabtn);
+        vars.success = function(res) {
+            this.response = res;
+            if ( !this.options.api_key && res.api_key ) {
+                this.options.api_key = res.api_key;
+            }
+            if ( !this.options.username && res.username ) {
+                this.options.username = res.username;
+            }
+            typeof o.success == 'function' ? o.success(res) : false;
         }
-    });
-})();
+        typeof o.error == 'function' ? vars.error = o.error : false;
+        $.ajax(vars);
+    },
+    register: function(o) {
+        // o should be an object containing a data object with email, profession, username
+        // and also a success function and error function if required
+        this.send('register',o);
+    },
+    status: function(o) {
+        //this.send('status',o);
+        // TODO: annoying behaviour of CORS on POST means this is being fugded as a JSONP GET for now
+        this.options.api_key && !o.data.api_key ? o.data.api_key = this.options.api_key : false;
+        var vars = {
+            type: 'GET',
+            url: this.options.api + '/status',
+            cache: false,
+            context: this,
+            dataType: 'JSONP',
+            data: o.data,
+            success: function(res) {
+                this.response = res;
+                typeof o.success == 'function' ? o.success(res) : false;
+            }
+        }
+        typeof o.error == 'function' ? vars.error = o.error : false;
+        $.ajax(vars);
+    },
+    blocked: function(o,rid) {
+        var t = 'blocked';
+        if ( rid ) {
+            t += '/' + rid;
+        }
+        this.send(t,o);
+    },
+    wishlist: function(o) {
+        this.send('wishlist',o);
+    }
+    // TODO: add the processor API route
+}
 
+var successfunctions = {
+    register: function(data) {
+    },
+    status: function(data) {
+        console.log(data);
+    },
+    blocked: function(data) {
+    },
+    wishlist: function(data) {
+    }
+}
+
+var OaButton = new oabutton({api_key: '44ab0972-3a56-4d1d-ba10-c98c5342ac1d'});
+doiLinks.forEach(function(e,i,a) {
+	var data = {url: e};
+	OaButton.status({data: data, success: successfunctions.status});
+});
